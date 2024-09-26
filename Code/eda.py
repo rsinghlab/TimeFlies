df2dd95d88efc1f41ea028c40b625a2b1455c740
nikolaitennant <98777012+nikolaitennant@users.noreply.github.com>
2024-09-26 16:20:34 +0100

    added files

---
+++ Code/eda.py
@@ -0,0 +1,308 @@
+# eda_handler.py
+
+import pandas as pd
+import numpy as np
+from scipy import stats
+import scipy
+from Code.visuals import VisualizationTools
+
+
+class EDAHandler:
+    """
+    A class to handle Exploratory Data Analysis (EDA) on multiple datasets.
+
+    This class takes in multiple datasets and performs EDA on them, leveraging
+    configuration settings to customize the analysis.
+    """
+
+    def __init__(
+        self,
+        config,
+        path_manager,
+        adata,
+        adata_eval,
+        adata_original,
+        adata_corrected,
+        adata_eval_corrected,
+    ):
+        """
+        Initializes the EDAHandler with the given configuration and datasets.
+
+        Parameters:
+        - config (ConfigHandler): Configuration settings for the EDA.
+        - path_manager (PathManager): Path manager object to handle directory paths.
+        - adata (AnnData): Main AnnData object containing the dataset.
+        - adata_eval (AnnData): AnnData object for evaluation data.
+        - adata_original (AnnData): The original unfiltered AnnData object.
+        - adata_corrected (AnnData): The batch-corrected AnnData object.
+        - adata_eval_corrected (AnnData): The batch-corrected evaluation AnnData object.
+        """
+        self.config = config
+        self.path_manager = path_manager
+        self.adata = adata
+        self.adata_eval = adata_eval
+        self.adata_original = adata_original
+        self.adata_corrected = adata_corrected
+        self.adata_eval_corrected = adata_eval_corrected
+
+        # Initialize VisualizationTools with config and path_manager
+        self.visual_tools = VisualizationTools(
+            config=self.config, path_manager=self.path_manager
+        )
+
+    def _print_general_info(self, adata):
+        """
+        Print general information about the dataset including its shape,
+        variable names, and columns in 'obs'.
+
+        Args:
+            adata (AnnData): The input dataset.
+        """
+        print(f"\nData Shape: {adata.shape}")
+        print(f"\nVariable names: {adata.var_names}")
+        print(f"\nColumns in 'obs': {adata.obs.columns}")
+
+    def _display_class_info(self, adata, encoding_column, folder_name, dataset_name):
+        """
+        Display information about the unique classes and class counts in the dataset.
+
+        Args:
+            adata (AnnData): The input dataset.
+            encoding_column (str): Column used for encoding class labels.
+            folder_name (str): Name of the folder where visualizations will be saved.
+            dataset_name (str): Name of the dataset for labeling outputs.
+        """
+        unique_classes = np.unique(adata.obs[encoding_column])
+        class_mapping = {
+            class_name: f"Class {idx}" for idx, class_name in enumerate(unique_classes)
+        }
+
+        print("\nUnique Classes:")
+        for class_name in unique_classes:
+            print(f"{class_mapping[class_name]}: {class_name}")
+
+        num_classes = len(unique_classes)
+        print(f"\nNumber of unique classes: {num_classes}")
+
+        # Class distribution
+        class_counts = adata.obs[encoding_column].value_counts().sort_values()
+        print(f"\nClass Counts:\n{class_counts}")
+
+        # Plot class distribution
+        self.visual_tools.plot_class_distribution(
+            class_counts=class_counts,
+            file_name=f"{dataset_name}_class_distribution.png",
+            dataset=folder_name,
+            subfolder_name="EDA",
+        )
+
+    def _analyze_gene_statistics(self, adata, folder_name, dataset_name):
+        """
+        Perform statistical analysis and outlier detection for gene expression data.
+
+        Args:
+            adata (AnnData): The input dataset.
+            folder_name (str): Name of the folder where visualizations will be saved.
+            dataset_name (str): Name of the dataset for labeling outputs.
+        """
+        # Statistical summary of the first 10 genes
+        data_arr = adata.X.toarray() if scipy.sparse.issparse(adata.X) else adata.X
+        stats_df = self._generate_stat_summary(data_arr, adata.var_names)
+        print(f"\nStatistical Summary for the first 10 genes:\n{stats_df}")
+
+        # Outlier detection for the first 10 genes
+        num_outliers = self._detect_outliers(data_arr, threshold=3)
+        print(f"\nNumber of outliers in the gene expression data: {num_outliers}")
+
+        # Visualize gene expression overview
+        gene_expression_matrix = data_arr[:5, :5]
+        gene_expression_df = pd.DataFrame(
+            data=gene_expression_matrix,
+            index=adata.obs.index[:5],
+            columns=adata.var.index[:5],
+        )
+        self.visual_tools.create_styled_dataframe(
+            df=gene_expression_df,
+            subfolder_name="EDA",
+            dataset=folder_name,
+            file_name=f"{dataset_name}_sampled_gene_expression_overview.png",
+        )
+
+        self.visual_tools.create_styled_dataframe(
+            df=stats_df,
+            subfolder_name="EDA",
+            dataset=folder_name,
+            file_name=f"{dataset_name}_gene_expression_statistics.png",
+        )
+
+    def _handle_missing_values(self, adata):
+        """
+        Calculate and return the number of missing values in the dataset.
+
+        Args:
+            adata (AnnData): Input AnnData object.
+
+        Returns:
+            int: Number of missing values in the dataset.
+        """
+        if scipy.sparse.issparse(adata.X):
+            data_arr = adata.X.toarray()
+        else:
+            data_arr = adata.X
+        return np.isnan(data_arr).sum()
+
+    def _check_for_duplicates(self, df):
+        """
+        Checks for duplicate rows in the given DataFrame.
+
+        Args:
+            df (pd.DataFrame): Input DataFrame.
+
+        Returns:
+            num_duplicates (int): Number of duplicate rows.
+            duplicate_rows (pd.DataFrame): DataFrame of duplicate rows.
+        """
+        duplicate_rows = df.duplicated()
+        num_duplicates = duplicate_rows.sum()
+        return num_duplicates, df[duplicate_rows]
+
+    def _detect_outliers(self, data, threshold=3):
+        """
+        Detects outliers in the given data based on the z-score.
+
+        Args:
+            data (np.array): Input data.
+            threshold (float): The z-score threshold to define outliers.
+
+        Returns:
+            int: Number of outliers detected.
+        """
+        z_scores = np.abs(stats.zscore(data[:, :10], axis=0))
+        return np.where(z_scores > threshold)[0].shape[0]
+
+    def _generate_stat_summary(self, data, var_names, num_genes=10):
+        """
+        Generates a statistical summary for the first few genes.
+
+        Args:
+            data (np.array): Gene expression data.
+            var_names (pd.Index): Variable names (gene names) corresponding to the data columns.
+            num_genes (int): The number of genes to include in the summary (default: 10).
+
+        Returns:
+            pd.DataFrame: A DataFrame containing the statistical summary for the specified number of genes.
+        """
+        # Generate a statistical summary for the first `num_genes` genes
+        stats_df = (
+            pd.DataFrame(data[:, :num_genes], columns=var_names[:num_genes])
+            .describe()
+            .transpose()
+        )
+        return stats_df
+
+    def eda(self, adata, encoding_column, folder_name, dataset_name):
+        """
+        Perform exploratory data analysis on the dataset.
+
+        Args:
+            adata (AnnData): The input data for analysis.
+            encoding_column (str): Column used for encoding class labels.
+            folder_name (str): Name of the folder where visualizations will be saved.
+            dataset_name (str): Name of the dataset for labeling outputs.
+
+        Returns:
+            None
+        """
+        # Display the first 5 rows of the 'obs' dataframe
+        print("\nFirst 5 rows of 'obs' dataframe:")
+        print(adata.obs.head(5))
+
+        # Visualize the 'obs' dataframe
+        self.visual_tools.create_styled_dataframe(
+            df=adata.obs.head(5),
+            subfolder_name="EDA",
+            dataset=folder_name,
+            file_name=f"{dataset_name}_observation_df_overview.png",
+        )
+
+        # Print and visualize basic dataset information
+        self._print_general_info(adata)
+
+        # Visualize sparse vs dense matrices
+        self.visual_tools.visualize_sparse_vs_dense(
+            adata=adata,
+            subset_size=50,
+            head_size=5,
+            file_name=f"{dataset_name}_sparse_vs_dense.png",
+            dataset=folder_name,
+            subfolder_name="EDA",
+        )
+
+        # Handle duplicate rows using a utility function
+        adata.obs = adata.obs.reset_index(drop=False)
+        num_duplicates, duplicate_rows = self._check_for_duplicates(adata.obs)
+        print(f"\nNumber of duplicate rows in 'obs': {num_duplicates}")
+        if num_duplicates > 0:
+            print("Duplicate rows in 'obs':")
+            print(duplicate_rows)
+        adata.obs.set_index("index", inplace=True)
+
+        # Handle missing values using a utility function
+        missing_values = self._handle_missing_values(adata)
+        print(f"\nMissing Values: {missing_values}")
+
+        # Display unique classes and class counts
+        self._display_class_info(
+            adata=adata,
+            encoding_column=encoding_column,
+            folder_name=folder_name,
+            dataset_name=dataset_name,
+        )
+
+        # Perform statistical analysis and outlier detection
+        self._analyze_gene_statistics(
+            adata=adata, folder_name=folder_name, dataset_name=dataset_name
+        )
+
+    def run_eda(self):
+        """
+        Runs EDA based on the configuration settings, deciding whether to
+        analyze corrected or uncorrected datasets, depending on the batch correction.
+
+        Returns:
+            None
+        """
+        if self.config.DataParameters.BatchCorrection.enabled:
+            # Run EDA on batch-corrected datasets
+            self.eda(
+                adata=self.adata_corrected,
+                encoding_column=self.config.DataParameters.GeneralSettings.encoding_variable,
+                folder_name="Batch Training Data",
+                dataset_name="batch_train",
+            )
+            self.eda(
+                adata=self.adata_eval_corrected,
+                encoding_column=self.config.DataParameters.GeneralSettings.encoding_variable,
+                folder_name="Batch Evaluation Data",
+                dataset_name="batch_evaluation",
+            )
+        else:
+            # Run EDA on uncorrected datasets
+            self.eda(
+                adata=self.adata,
+                encoding_column=self.config.DataParameters.GeneralSettings.encoding_variable,
+                folder_name="Training Data",
+                dataset_name="train",
+            )
+            self.eda(
+                adata=self.adata_eval,
+                encoding_column=self.config.DataParameters.GeneralSettings.encoding_variable,
+                folder_name="Evaluation Data",
+                dataset_name="evaluation",
+            )
+            self.eda(
+                adata=self.adata_original,
+                encoding_column=self.config.DataParameters.GeneralSettings.encoding_variable,
+                folder_name="Original Data",
+                dataset_name="original",
+            )
---
+++ Code/interpreter.py
@@ -0,0 +1,345 @@
+# interpreter.py
+
+import os
+import numpy as np
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
+from sklearn.dummy import DummyClassifier
+import shap
+import pickle
+import logging
+
+
+class Prediction:
+    """
+    This class provides methods for evaluating and making predictions using a trained machine learning model.
+
+    Methods:
+        calculate_baseline_scores(y_true):
+            Calculates the baseline accuracy given a set of true labels.
+
+        evaluate_model(model, test_inputs, test_labels):
+            Evaluates the model on test data.
+
+        make_predictions(model, test_inputs):
+            Makes predictions on test data using the trained model.
+    """
+
+    @staticmethod
+    def calculate_baseline_scores(y_true):
+        """
+        Calculate the baseline accuracy given a set of true labels.
+
+        Args:
+            y_true (numpy.ndarray): An array of true labels.
+
+        Returns:
+            baseline_accuracy (float): The baseline accuracy.
+            baseline_precision (float): The baseline precision.
+            baseline_recall (float): The baseline recall.
+            baseline_f1 (float): The baseline f1 score.
+        """
+
+        # Create a dummy classifier that will predict the most frequent class
+        dummy_clf = DummyClassifier(strategy="most_frequent")
+        dummy_clf.fit(y_true, y_true)
+        baseline_preds = dummy_clf.predict(y_true)
+
+        # Calculate accuracy, precision, recall, and f1 score
+        baseline_accuracy = accuracy_score(y_true, baseline_preds)
+        baseline_precision = precision_score(y_true, baseline_preds, average="macro")
+        baseline_recall = recall_score(y_true, baseline_preds, average="macro")
+        baseline_f1 = f1_score(y_true, baseline_preds, average="macro")
+
+        return baseline_accuracy, baseline_precision, baseline_recall, baseline_f1
+
+    @staticmethod
+    def evaluate_model(model, test_inputs, test_labels):
+        """
+        Evaluate the model on test data.
+
+        Args:
+            model (Model): A trained TensorFlow model.
+            test_inputs (numpy.ndarray): Test input data.
+            test_labels (numpy.ndarray): Test labels.
+
+        Returns:
+            test_loss (float): The value of the test loss for the input data.
+            test_acc (float): The value of the test accuracy for the input data.
+            test_auc (float): The value of the test AUC for the input data.
+        """
+
+        # Evaluate the model on test data
+        test_loss, test_acc, test_auc = model.evaluate(test_inputs, test_labels)
+        return test_loss, test_acc, test_auc
+
+    @staticmethod
+    def make_predictions(model, test_inputs):
+        """
+        Make predictions on test data using the trained model.
+
+        Args:
+            model (Model): A trained TensorFlow model.
+            test_inputs (numpy.ndarray): Test input data.
+
+        Returns:
+            y_pred (numpy.ndarray): An array of predicted probabilities for the input data.
+            y_pred_binary (numpy.ndarray): An array of binary predictions for the input data.
+        """
+
+        # Make predictions using the trained model
+        y_pred = model.predict(test_inputs)
+
+        # Convert the predicted probabilities into binary predictions
+        y_pred_binary = np.argmax(y_pred, axis=1)
+
+        return y_pred, y_pred_binary
+
+
+class Interpreter:
+    """
+    A class to handle model interpretation using SHAP.
+    """
+
+    def __init__(
+        self,
+        config,
+        model,
+        test_data,
+        test_labels,
+        label_encoder,
+        reference_data,
+        path_manager,
+    ):
+        """
+        Initializes the Interpreter with the given configuration and model.
+
+        Parameters:
+        - config (Config): The configuration object.
+        - model (object): The best model to be interpreted.
+        - test_data (numpy.ndarray): The test data used during model training.
+        - test_labels (numpy.ndarray): The labels for the test data.
+        - label_encoder (LabelEncoder): The label encoder used during training.
+        - reference_data (numpy.ndarray): The reference data used during model training.
+        """
+        self.config = config
+        self.model = model  # Best model for SHAP analysis
+        self.test_data = test_data  # Best model's test data
+        self.test_labels = test_labels  # Best model's test labels
+        self.label_encoder = label_encoder
+        self.reference_data = reference_data  # Best model's reference data
+        self.path_manager = path_manager
+
+        self.shap_dir = self.path_manager.get_visualization_directory(
+            analysis_type="Analysis",
+            subfolder="SHAP",
+        )
+
+        # Set the filepath for SHAP values
+        self.shap_values_filepath = os.path.join(self.shap_dir, "shap_values.pkl")
+
+    def compute_or_load_shap_values(self):
+        """
+        Compute or load SHAP values for model interpretation based on the configuration.
+
+        Returns:
+        - tuple: A tuple containing the SHAP values and the corresponding SHAP test data.
+        """
+        shap_values = None
+
+        if self.config.FeatureImportanceAndVisualizations.load_SHAP:
+            # Attempt to load SHAP values from disk
+            try:
+                shap_values = self.load_shap_values()
+                squeezed_test_data = (
+                    self.test_data
+                    if self.test_data.ndim <= 2
+                    else np.squeeze(self.test_data, axis=1)
+                )
+                logging.info("Loaded SHAP values and data from disk.")
+            except Exception as e:
+                logging.error(f"Error loading SHAP values: {e}")
+                logging.info("Computing SHAP values instead.")
+                shap_values, squeezed_test_data = self.compute_shap_values()
+                self.save_shap_values(shap_values)
+        else:
+            # Compute SHAP values
+            shap_values, squeezed_test_data = self.compute_shap_values()
+            self.save_shap_values(shap_values)
+            logging.info("Computed and saved SHAP values.")
+
+        return shap_values, squeezed_test_data
+
+    def compute_shap_values(self):
+        """
+        Compute SHAP values for model interpretation.
+
+        Returns:
+        - tuple: A tuple containing the SHAP values and the corresponding SHAP test data.
+        """
+        # Use the entire test data and reference data for SHAP computation
+        shap_test_data = self.test_data
+        shap_reference_data = self.reference_data
+
+        # Access the model type from the configuration
+        model_type = self.config.DataParameters.GeneralSettings.model_type.lower()
+
+        # Determine the explainer to use based on the model type
+        if model_type in ["mlp", "cnn"]:
+            # For neural network models, use GradientExplainer
+            explainer = shap.GradientExplainer(self.model, shap_reference_data)
+        elif model_type in ["xgboost", "randomforest"]:
+            # For tree-based models, use TreeExplainer
+            explainer = shap.TreeExplainer(self.model)
+        else:
+            # For linear models, use LinearExplainer
+            explainer = shap.LinearExplainer(self.model, shap_reference_data)
+
+        # Compute SHAP values
+        shap_values = explainer.shap_values(shap_test_data)
+
+        # Adjust SHAP values and test data shapes if necessary
+        if isinstance(shap_values, list):
+            squeezed_shap_values = [
+                np.squeeze(val, axis=1) if val.ndim > 3 else val for val in shap_values
+            ]
+        else:
+            squeezed_shap_values = (
+                np.squeeze(shap_values, axis=1) if shap_values.ndim > 3 else shap_values
+            )
+
+        squeezed_test_data = (
+            shap_test_data
+            if shap_test_data.ndim <= 2
+            else np.squeeze(shap_test_data, axis=1)
+        )
+
+        return squeezed_shap_values, squeezed_test_data
+
+    def save_shap_values(self, shap_values):
+        """
+        Save SHAP values, model metadata, and the data used during SHAP computation.
+
+        Args:
+            shap_values (list or numpy.ndarray): The SHAP values to be saved.
+        """
+        # Collect model and data metadata
+        model_weights_hash = self._get_model_weights_hash()
+        metadata = {
+            "model_type": self.config.DataParameters.GeneralSettings.model_type,
+            "model_config": (
+                self.model.get_config() if hasattr(self.model, "get_config") else None
+            ),
+            "model_weights_hash": model_weights_hash,
+            "test_data_hash": hash(self.test_data.tobytes()),
+            "reference_data_hash": hash(self.reference_data.tobytes()),
+        }
+
+        # Save SHAP values, metadata, and the data
+        with open(self.shap_values_filepath, "wb") as f:
+            pickle.dump(
+                {
+                    "shap_values": shap_values,
+                    "metadata": metadata,
+                    "reference_data": self.reference_data,
+                    "test_data": self.test_data,
+                },
+                f,
+            )
+        logging.info(
+            f"SHAP values, metadata, and data saved to {self.shap_values_filepath}"
+        )
+
+    def load_shap_values(self):
+        """
+        Load previously saved SHAP values along with the reference data and test data used during SHAP computation.
+
+        Returns:
+            The loaded SHAP values.
+        """
+        with open(self.shap_values_filepath, "rb") as f:
+            data = pickle.load(f)
+
+        loaded_shap_values = data["shap_values"]
+        saved_metadata = data["metadata"]
+        loaded_reference_data = data["reference_data"]
+        loaded_test_data = data["test_data"]
+
+        # Collect current model metadata and data hashes before updating data
+        model_weights_hash = self._get_model_weights_hash()
+        current_metadata = {
+            "model_type": self.config.DataParameters.GeneralSettings.model_type,
+            "model_config": (
+                self.model.get_config() if hasattr(self.model, "get_config") else None
+            ),
+            "model_weights_hash": model_weights_hash,
+            "test_data_hash": (
+                hash(self.test_data.tobytes()) if self.test_data is not None else None
+            ),
+            "reference_data_hash": (
+                hash(self.reference_data.tobytes())
+                if self.reference_data is not None
+                else None
+            ),
+        }
+
+        # Verify model metadata
+        model_match = (
+            saved_metadata["model_weights_hash"]
+            == current_metadata["model_weights_hash"]
+        )
+
+        # Verify data metadata
+        test_data_match = (
+            saved_metadata["test_data_hash"] == current_metadata["test_data_hash"]
+        )
+        reference_data_match = (
+            saved_metadata["reference_data_hash"]
+            == current_metadata["reference_data_hash"]
+        )
+
+        # Perform consistency checks
+        if not model_match:
+            logging.warning(
+                "The model used for SHAP computation does not match the current best model."
+            )
+        if not test_data_match:
+            logging.warning(
+                "The test data used for SHAP computation does not match the current test data."
+            )
+        if not reference_data_match:
+            logging.warning(
+                "The reference data used for SHAP computation does not match the current reference data."
+            )
+
+        if model_match and test_data_match and reference_data_match:
+            logging.info(
+                "The model and data used for SHAP computation match the current best model and data."
+            )
+        else:
+            logging.warning("Consider recomputing SHAP values for accurate results.")
+
+        # Now update self.test_data and self.reference_data
+        self.test_data = loaded_test_data
+        self.reference_data = loaded_reference_data
+
+        logging.info(f"SHAP values and data loaded from {self.shap_values_filepath}")
+        return loaded_shap_values
+
+    def _get_model_weights_hash(self):
+        """
+        Get a hash of the model's weights for consistency checks.
+
+        Returns:
+            int: The hash of the model's weights.
+        """
+        if hasattr(self.model, "get_weights"):
+            # For Keras models
+            weights = self.model.get_weights()
+            weights_bytes = b"".join([w.tobytes() for w in weights])
+            return hash(weights_bytes)
+        else:
+            # For other models like scikit-learn models
+            import pickle
+
+            model_bytes = pickle.dumps(self.model)
+            return hash(model_bytes)
